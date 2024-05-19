0c7d62f09b215693849e6fd4695ba63df8883041
Emmanuel Salcedo <davidsalcedo388@gmail.com>
2024-05-18 22:23:26 -0400

    chore: Update ESLint and Prettier configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,8 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    "prettier",
+    "prettier/@typescript-eslint"
   ],
   root: true,
   env: {
@@ -18,6 +20,7 @@ module.exports = {
   ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
+    "prettier/prettier": ["warn", {"endOfLine": "auto"}],
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
