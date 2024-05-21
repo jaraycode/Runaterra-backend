b08d54b125adb4f143c654bb1ab5d30de6790960
jaraycode <jonasaray12@gmail.com>
2024-05-21 11:02:43 -0400

    ENDPOINTS dptos
    Se desarrollo la capacidad de crear las tablas en la base de datos por medio del ORM, obtener todos los departamentos, crearlos y obtener uno específico

--- src/core/dptos/dptos.module.ts
+++ src/core/dptos/dptos.module.ts
@@ -1,8 +1,23 @@
 import { Module } from "@nestjs/common";
 import { DptosService } from "./services/dptos.service";
 import { DptosController } from "./dptos.controller";
+import { envData } from "@src/config/typeorm";
+import { DbValidatorsModule } from "@youba/nestjs-dbvalidator";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Dpto } from "./entities/dpto.entity";
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Dpto]),
+    DbValidatorsModule.register({
+      type: "postgres",
+      host: envData.DATABASE_HOST,
+      port: parseInt(envData.DATABASE_PORT),
+      username: envData.DATABASE_USERNAME,
+      password: envData.DATABASE_PASSWORD,
+      database: envData.DATABASE_NAME,
+    }),
+  ],
   controllers: [DptosController],
   providers: [DptosService],
 })
