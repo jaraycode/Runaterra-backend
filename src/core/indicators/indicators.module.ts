de2a7f8227b298bbfac4f91b55258103b7136c74
jaraycode <jonasaray12@gmail.com>
2024-05-23 15:25:30 -0400

    fix: Module indicators
    It was taken off the dbValidatorModule which it does not have an use case for it's decorators for the resource indicators

--- src/core/indicators/indicators.module.ts
+++ src/core/indicators/indicators.module.ts
@@ -9,17 +9,7 @@ import { ConfigService } from "nestjs-config";
 const configService = new ConfigService();
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Indicator]),
-    DbValidatorsModule.register({
-      type: "postgres",
-      host: configService.get("DATABASE_HOST"),
-      port: configService.get("DATABASE_PORT"),
-      username: configService.get("DATABASE_USERNAME"),
-      password: configService.get("DATABASE_PASSWORD"),
-      database: configService.get("DATABASE_NAME"),
-    }),
-  ],
+  imports: [TypeOrmModule.forFeature([Indicator])],
   controllers: [IndicatorsController],
   providers: [IndicatorsService],
 })
