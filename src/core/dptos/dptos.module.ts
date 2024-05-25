44e9f24a3dcf76ceda41ac66a621b775e0b6e910
Emmanuel Salcedo <davidsalcedo388@gmail.com>
2024-05-24 22:46:44 -0400

    Fix : bug extraño

--- src/core/dptos/dptos.module.ts
+++ src/core/dptos/dptos.module.ts
@@ -1,23 +1,11 @@
 import { Module } from "@nestjs/common";
 import { DptosService } from "./services/dptos.service";
 import { DptosController } from "./dptos.controller";
-import { envData } from "@src/config/typeorm";
-import { DbValidatorsModule } from "@youba/nestjs-dbvalidator";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Dpto } from "./entities/dpto.entity";
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Dpto]),
-    DbValidatorsModule.register({
-      type: "postgres",
-      host: envData.DATABASE_HOST,
-      port: parseInt(envData.DATABASE_PORT),
-      username: envData.DATABASE_USERNAME,
-      password: envData.DATABASE_PASSWORD,
-      database: envData.DATABASE_NAME,
-    }),
-  ],
+  imports: [TypeOrmModule.forFeature([Dpto])],
   controllers: [DptosController],
   providers: [DptosService],
   exports: [DptosService],
