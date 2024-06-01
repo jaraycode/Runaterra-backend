0f6fb8bfb4283f006dc39cc8cccc6cabbf2bb9e7
Emmanuel Salcedo <davidsalcedo388@gmail.com>
2024-06-01 17:37:24 -0400

    chore: Update ValidationPipe options in main.ts

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,13 @@ async function bootstrap() {
 
   app.setGlobalPrefix("api/v1");
 
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   app.useGlobalFilters(new ExceptionsFilter(appConfig));
 
