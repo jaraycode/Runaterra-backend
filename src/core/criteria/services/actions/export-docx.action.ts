a3ac611eef586aea07299671997ece3bb75336ca
Nannajpg <abcadena.19@est.ucab.edu.ve>
2024-06-30 17:31:44 -0400

    estructura del docx

--- src/core/criteria/services/actions/export-docx.action.ts
+++ src/core/criteria/services/actions/export-docx.action.ts
@@ -1,44 +1,141 @@
 import { Injectable } from "@nestjs/common";
 import * as fs from "fs";
-import { Document, Packer, Paragraph, TextRun } from "docx";
+import { AlignmentType, Document, HeadingLevel, ImageRun, Packer, Paragraph, TextRun } from "docx";
 
 @Injectable()
 export class ExportDocxAction {
   constructor() {}
 
   async execute(criteriaId: number): Promise<Buffer> {
+    const buildActivitiesParagraphs = () => {
+      let paragraphArray = [];
+
+      for (var i = 0; i < 1; i++) {
+        paragraphArray.push(
+          new Paragraph({
+            text: "Departamento #" + i,
+            heading: HeadingLevel.HEADING_2,
+            alignment: AlignmentType.LEFT,
+          }),
+
+          new Paragraph({
+            text: "Aporte #" + i,
+            heading: HeadingLevel.HEADING_3,
+            alignment: AlignmentType.LEFT,
+            style: "IntenseQuote",
+          }),
+
+          new Paragraph({
+            children: [
+              new TextRun({
+                text: "Descripción del aporte",
+                bold: false,
+                italics: true,
+              }),
+            ],
+          }),
+
+          new Paragraph({
+            text: "Fotos",
+            heading: HeadingLevel.HEADING_3,
+            alignment: AlignmentType.LEFT,
+            style: "IntenseQuote",
+          }),
+
+          new Paragraph({
+            children: [
+              new TextRun({
+                text: "▢",
+              }),
+              new TextRun({
+                text: "Descripción de la foto",
+                bold: false,
+                italics: true,
+              }),
+            ],
+          }),
+
+          new Paragraph({
+            text: "Archivos",
+            heading: HeadingLevel.HEADING_3,
+            alignment: AlignmentType.LEFT,
+            style: "IntenseQuote",
+          }),
+
+          new Paragraph({
+            children: [
+              new TextRun({
+                text: "archivo.com",
+                bold: false,
+                style: "Hyperlink",
+              }),
+            ],
+          }),
+
+          new Paragraph({
+            children: [
+              new TextRun({
+                text: "Descripción del archivo",
+                bold: false,
+                italics: true,
+              }),
+            ],
+          }),
+
+          new Paragraph({
+            text: "Links",
+            heading: HeadingLevel.HEADING_3,
+            alignment: AlignmentType.LEFT,
+            style: "IntenseQuote",
+          }),
+
+          new Paragraph({
+            children: [
+              new TextRun({
+                text: "link.com",
+                bold: false,
+                style: "Hyperlink",
+              }),
+            ],
+          }),
+
+          new Paragraph({
+            children: [
+              new TextRun({
+                text: "Descripción del link",
+                bold: false,
+                italics: true,
+              }),
+            ],
+          }),
+        );
+      }
+
+      return paragraphArray;
+    };
+
     const doc = new Document({
-        sections: [
-            {
-                properties: {},
-                children: [
-                    new Paragraph({
-                        children: [
-                            new TextRun("Hello World"),
-                            new TextRun({
-                                text: "Foo Bar",
-                                bold: true,
-                            }),
-                            new TextRun({
-                                text: "\tGithub is the best",
-                                bold: true,
-                            }),
-                        ],
-                    }),
-                ],
-            },
-        ],
+      sections: [
+        {
+          properties: {},
+          children: [
+            new Paragraph({
+              text: "Indicador #",
+              heading: HeadingLevel.HEADING_1,
+              alignment: AlignmentType.CENTER,
+            }),
+            new Paragraph({
+              text: "Criterio #",
+              heading: HeadingLevel.HEADING_2,
+              alignment: AlignmentType.CENTER,
+            }),
+            ...buildActivitiesParagraphs(),
+          ],
+        },
+      ],
     });
 
     const buffer = await Packer.toBuffer(doc);
     return buffer;
-
-    // Used to export the file into a .docx file
-    /*Packer.toBuffer(doc).then((buffer) => {
-        fs.writeFileSync(`criterio-greenie-panel-${criteriaId}.docx`, buffer);
-    });*/
   }
 }
-
-
-
