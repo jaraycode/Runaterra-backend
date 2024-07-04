5b07dc662616cb5f1c8ab71f8da213dd900eaa6e
Emmanuel Salcedo <davidsalcedo388@gmail.com>
2024-07-03 22:28:45 -0400

    feat: add contribution information to docx rendering

--- src/core/criteria/services/actions/render-contribution.ts
+++ src/core/criteria/services/actions/render-contribution.ts
@@ -1,15 +1,95 @@
 import { Injectable } from "@nestjs/common";
 import { AlignmentType, HeadingLevel, Paragraph, TextRun } from "docx";
 import { Criteria } from "../../entities/criteria.entity";
+import { Contribution } from "@src/core/contributions/entities/contribution.entity";
+import { Dpto } from "@src/core/dptos/entities/dpto.entity";
+
+interface DptoDetailed extends Dpto {
+  contributionsOnCriteria: Contribution[];
+}
 
 @Injectable()
 export class RenderContributions {
   constructor() {}
 
+  private getProccessedDepartments(contributions: Contribution[]): Promise<Dpto[]> {
+    const departments = new Set();
+
+    contributions.forEach((contribution) => {
+      const contribution;
+
+      if (departments[departmentName]) {
+        departments.add({
+          ...contribution.user.department,
+          contributionsOnCriteria: [contribution],
+        });
+      } else {
+      }
+    });
+
+    return departments;
+  }
+
   render(criteria: Criteria) {
     try {
       let paragraphArray = [];
-      const processedDepartments = new Set<string>();
+      const departments = this.getProccessedDepartments(criteria.categories.contribution);
+
+      departments.forEach((department) => {
+        paragraphArray.push(
+          new Paragraph({
+            text: `Departamento ${[department]}`,
+            heading: HeadingLevel.HEADING_2,
+            alignment: AlignmentType.LEFT,
+          }),
+        );
+
+        paragraphArray.push(
+          new Paragraph({
+            text: `Aporte #${i + 1}:`,
+            heading: HeadingLevel.HEADING_3,
+            alignment: AlignmentType.LEFT,
+            style: "IntenseQuote",
+          }),
+
+          new Paragraph({
+            children: [
+              new TextRun({
+                text: criteria.categories.contribution[i].description,
+                bold: false,
+                italics: true,
+              }),
+            ],
+          }),
+
+          new Paragraph({
+            text: "Fotos",
+            heading: HeadingLevel.HEADING_3,
+            alignment: AlignmentType.LEFT,
+            style: "IntenseQuote",
+          }),
+
+          new Paragraph({
+            children: [
+              new TextRun({
+                text: "▢",
+              }),
+              new TextRun({
+                text: "Descripción de la foto",
+                bold: false,
+                italics: true,
+              }),
+            ],
+          }),
+
+          new Paragraph({
+            text: "Archivos",
+            heading: HeadingLevel.HEADING_3,
+            alignment: AlignmentType.LEFT,
+            style: "IntenseQuote",
+          }),
+        );
+      });
 
       for (var i = 0; i < criteria.categories.contribution.length; i++) {
         const departmentName = criteria.categories.contribution[i].user.department.name;
@@ -17,58 +97,6 @@ export class RenderContributions {
         if (!processedDepartments.has(departmentName)) {
           processedDepartments.add(departmentName);
 
-          paragraphArray.push(
-            new Paragraph({
-              text: `Departamento #${i + 1}: ${departmentName}`,
-              heading: HeadingLevel.HEADING_2,
-              alignment: AlignmentType.LEFT,
-            }),
-          );
-          paragraphArray.push(
-            new Paragraph({
-              text: `Aporte #${i + 1}:`,
-              heading: HeadingLevel.HEADING_3,
-              alignment: AlignmentType.LEFT,
-              style: "IntenseQuote",
-            }),
-
-            new Paragraph({
-              children: [
-                new TextRun({
-                  text: criteria.categories.contribution[i].description,
-                  bold: false,
-                  italics: true,
-                }),
-              ],
-            }),
-
-            new Paragraph({
-              text: "Fotos",
-              heading: HeadingLevel.HEADING_3,
-              alignment: AlignmentType.LEFT,
-              style: "IntenseQuote",
-            }),
-
-            new Paragraph({
-              children: [
-                new TextRun({
-                  text: "▢",
-                }),
-                new TextRun({
-                  text: "Descripción de la foto",
-                  bold: false,
-                  italics: true,
-                }),
-              ],
-            }),
-
-            new Paragraph({
-              text: "Archivos",
-              heading: HeadingLevel.HEADING_3,
-              alignment: AlignmentType.LEFT,
-              style: "IntenseQuote",
-            }),
-          );
           criteria.categories.contribution[i].files.forEach((file) => {
             paragraphArray.push(
               new Paragraph({
